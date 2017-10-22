6c1f96b4219812767d8d15d67cc1c5981b62d4d7
Manuj Bhandari <manujbh@gmail.com>
2017-10-22 11:58:55 -0700

    Time: 20171022 11:57 am

--- myKivyApp01/myKivyApp01/myKivyApp01.py
+++ myKivyApp01/myKivyApp01/myKivyApp01.py
@@ -1,28 +1,20 @@
-
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
 
 class RootContainer(BoxLayout):
+    instance = ObjectProperty(None)
 
     def clickAction1(self, instance):
         # instead of accepting "msg". identify which button was pressed and query from DB the appropriate content. Then start the chain of events for updates.
         # update "lbl1" and "lbl2" with the appropriate content. 
         #self.lbl1.text = msg
         print(instance)
-
-    def print_ids(self, *args):
-        print("\nids:")
-        for widget in self.walk():
-            print("{} -> {}".format(widget, widget.id))
-
-    def get_id(self,  instance):
-        for id, widget in instance.parent.ids.items():
-            if widget.__self__ == instance:
-                return id
+        print(instance.text)
 
 class MBApp(App):
     # this is a native function from Kivy to actually build the app using KV files
