658c928fd8783a9f2a2b45890123431db4d073a7
Manuj Bhandari <manujbh@gmail.com>
2017-10-22 15:16:26 -0700

    added button identifier. Added SQLite db.

--- myKivyApp01/myKivyApp01/myKivyApp01.py
+++ myKivyApp01/myKivyApp01/myKivyApp01.py
@@ -1,10 +1,14 @@
 import kivy
+import sqlite3
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
+
+conn = sqlite3.connect('C:\Users\USER-MB\source\GIT_Repos\myKivyApp01\myKivyAppDB.db')
+c = conn.cursor()
 
 class RootContainer(BoxLayout):
     instance = ObjectProperty(None)
@@ -14,7 +18,13 @@ class RootContainer(BoxLayout):
         # update "lbl1" and "lbl2" with the appropriate content. 
         #self.lbl1.text = msg
         print(instance)
-        print(instance.text)
+        print(instance.text[0:5])
+        temp = c.execute('''select enhancement from moderateBaseline where id=1''')
+        print(temp)
+        # Query table
+        #c.execute('''CREATE TABLE stocks
+             #(date text, trans text, symbol text, qty real, price real)''')
+        
 
 class MBApp(App):
     # this is a native function from Kivy to actually build the app using KV files
