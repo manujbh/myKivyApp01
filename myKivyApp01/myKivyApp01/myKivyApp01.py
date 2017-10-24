1eff217ff6be62b85771c68453c011446b657e3b
Manuj Bhandari <manujbh@gmail.com>
2017-10-23 20:59:56 -0700

    Time: 20171023 8:59 pm

--- myKivyApp01/myKivyApp01/myKivyApp01.py
+++ myKivyApp01/myKivyApp01/myKivyApp01.py
@@ -6,7 +6,11 @@ from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
+from kivy.uix.scrollview import ScrollView
+from kivy.core.window import Window
+from kivy.app import runTouchApp
 
 sqlite_db = "C:/Users/USER-MB/source/GIT_Repos/myKivyApp01/myKivyAppDB.db"
 conn = sqlite3.connect(sqlite_db)
@@ -18,21 +22,25 @@ class RootContainer(BoxLayout):
     def clickAction1(self, instance):
         #print(instance)
         #identify the button pressed
+        t = (1,)
         buttonText = (instance.text[0:2],)
         print(buttonText)
         
         #use button text to query all related button info from DB
-        #c.execute('select * from moderateBaseline where id=?',buttonText)
-        #execute query
-        t = (1,)
-        c.execute('select * from moderateBaseline where id=?',t)
+        #c.execute('select * from moderateBaseline where id=?',t)
+        c.execute('select * from moderateBaseline where family=?',buttonText)
+
         #fetch results of query, fetchall() can only be used once
         results = c.fetchall()
+        print(results)
         print(results[0][3])
-        self.lbl1.text = results[0][4]
-        self.lbl2.text = results[0][2]
-        
+        self.lbl1.text = results[1][4]
+        self.lbl2.text = '\n'.join([x[2] for x in results])
+        self.lbl5.text = '\n'.join([x[3] for x in results])
 
+        # dynamically add buttons 
+        #for x in L:
+            #x = self.add_widget(Button(text = os.listdir('saves')[x]))
 
 class MBApp(App):
     # this is a native function from Kivy to actually build the app using KV files
